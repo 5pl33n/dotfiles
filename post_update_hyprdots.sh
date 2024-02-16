b712b754f3df6699ac7e4cd01bb0b1bc1cbd8411
Luca-Bragante-Sunnyvale <luca.bragante@ext.tecnomat.it>
2024-02-16 19:43:09 +0100

    Added post hyprdots update script

---
+++ post_update_hyprdots.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp code/keybindings.json ~/.config/Code/User/
+cp code/settings.json ~/.config/Code/User/
+cp hypr/userprefs.conf ~/.config/hypr/userprefs.conf
+cp kitty.conf ~/.config/kitty/kitty.conf
+cat waybar/config.ctl >> ~/.config/waybar/config.ctl
