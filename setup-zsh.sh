f54a7ced4b75a3c27b7c8a0d05ebd47de2e3ac89
spleen <luca.bragante@gmail.com>
2024-05-17 08:58:29 +0200

    Startred setup script

---
+++ setup-zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo git clone clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color
+sudo git clone clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions
+sudo git clone clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
