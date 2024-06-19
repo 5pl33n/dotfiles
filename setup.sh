7d9d558534e76d9b6d21fdd11c807c2005f5a1fc
5pl33n <luca.bragante@gmail.com>
2024-06-19 23:07:54 +0200

    Feat: added tmux plugin

--- setup.sh
+++ setup.sh
@@ -11,3 +11,5 @@ rm -rf yay
 sudo git clone clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color
 sudo git clone clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions
 sudo git clone clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
