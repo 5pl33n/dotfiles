9dbf53495bbe2e8a7e882d991a3f786f0a2a2e6e
5pl33n <luca.bragante@gmail.com>
2024-07-01 18:43:07 +0200

    Fix: typo

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ makepkg -si
 cd ..
 rm -rf yay
 
-yay -S zsh zsh-theme-powerlevel10k-git xdg-desktop-portal-gtk xdg-desktop-portal-hyprland wlogout wireplumber waybar tmux swaylock-effects-git swaybg dunst eza htop kitty neovim ripgrep firefox ttf-cascadia-mono-nerd
+yay -S zsh oh-my-zsh-git zsh-theme-powerlevel10k-git xdg-desktop-portal-gtk xdg-desktop-portal-hyprland wlogout wireplumber waybar tmux swaylock-effects-git swaybg dunst eza htop kitty neovim ripgrep firefox ttf-cascadia-mono-nerd
 
 git clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color
 git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions
@@ -16,3 +16,4 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp -a /home/spleen/5pl33n/dotfiles/. /home/spleen/
 
+chsh /usr/bin/zsh
