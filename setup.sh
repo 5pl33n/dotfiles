ede0ad576f26690b4360e18c1f0325a1aaf77ddb
5pl33n <luca.bragante@gmail.com>
2024-07-01 19:55:04 +0200

    Fix: typo

--- setup.sh
+++ setup.sh
@@ -8,9 +8,9 @@ rm -rf yay
 
 yay -S zsh oh-my-zsh-git zsh-theme-powerlevel10k-git xdg-desktop-portal-gtk xdg-desktop-portal-hyprland wlogout wireplumber waybar tmux swaylock-effects-git swaybg dunst eza htop kitty neovim ripgrep firefox ttf-cascadia-mono-nerd
 
-git clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color
-git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
