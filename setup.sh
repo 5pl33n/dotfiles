db8f11c8fb13188a69e903bcd628e5f69381c64b
5pl33n <luca.bragante@gmail.com>
2024-07-01 20:40:55 +0200

    some fix

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ makepkg -si
 cd ..
 rm -rf yay
 
-yay -S zsh oh-my-zsh-git zsh-theme-powerlevel10k-git hyprland xdg-desktop-portal-gtk xdg-desktop-portal-hyprland wlogout wireplumber waybar tmux swaylock-effects-git swaybg dunst eza htop kitty neovim ripgrep firefox ttf-cascadia-mono-nerd --noconfirm
+yay -S --noconfirm zsh oh-my-zsh-git zsh-theme-powerlevel10k-git hyprland xdg-desktop-portal-gtk xdg-desktop-portal-hyprland wlogout wireplumber waybar tmux swaylock-effects-git swaybg dunst eza htop kitty neovim ripgrep firefox ttf-cascadia-mono-nerd 
 
 sudo git clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions
@@ -16,4 +16,4 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp -a /home/spleen/5pl33n/dotfiles/. /home/spleen/
 
-chsh /usr/bin/zsh
+chsh -s /usr/bin/zsh
