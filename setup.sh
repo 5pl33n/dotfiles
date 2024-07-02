c7595096736af10c5408d437613d246f3f3c2e78
5pl33n <luca.bragante@gmail.com>
2024-07-02 09:35:18 +0200

    sddm

--- setup.sh
+++ setup.sh
@@ -14,13 +14,13 @@ sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/o
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-cp -a /home/spleen/5pl33n/dotfiles/. /home/spleen/
+cp -a ~/5pl33n/dotfiles/. ~/
 
 chsh -s /usr/bin/zsh
 
-wget -O /usr/share/sddm/themes/catppuccin-mocha.zip https://github.com/catppuccin/sddm/releases/download/v1.0.0/catppuccin-mocha.zip
+sudo wget -O /usr/share/sddm/themes/catppuccin-mocha.zip https://github.com/catppuccin/sddm/releases/download/v1.0.0/catppuccin-mocha.zip
 cd /usr/share/sddm/themes/ 
-unzip catppuccin-mocha.zip
+sudo unzip catppuccin-mocha.zip
 rm catppuccin-mocha.zip
 cd
 
