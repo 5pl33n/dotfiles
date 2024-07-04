1cd7606edafd28e8856b77616be396a9e6369670
5pl33n <luca.bragante@gmail.com>
2024-07-04 18:55:45 +0200

    Feat: damn

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ makepkg -si
 cd ..
 rm -rf yay
 
-yay -S --noconfirm zsh oh-my-zsh-git zsh-theme-powerlevel10k-git hyprland xdg-desktop-portal-gtk xdg-desktop-portal-hyprland wlogout wireplumber waybar tmux swaylock-effects-git swaybg dunst eza htop kitty neovim ripgrep firefox ttf-cascadia-code-nerd qt6-svg qt6-declarative sddm unzip
+yay -S --noconfirm zsh oh-my-zsh-git zsh-theme-powerlevel10k-git hyprland xdg-desktop-portal-gtk xdg-desktop-portal-hyprland wlogout wireplumber waybar tmux swaylock-effects-git swaybg dunst eza htop kitty neovim ripgrep firefox ttf-cascadia-code-nerd qt6-svg qt6-declarative sddm unzip polkit-kde-agent qt5-wayland qt6-wayland
 
 sudo git clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions
