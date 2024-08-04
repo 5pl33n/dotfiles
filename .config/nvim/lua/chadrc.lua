4d6390e6ce967739457472d1e0ce7e442ce470d5
5pl33n <luca.bragante@gmail.com>
2024-08-05 00:19:46 +0200

    1E1E2E

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,4 +1,4 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
@@ -9,12 +9,15 @@ M.ui = {
 
   statusline = {
     theme = "minimal",
-    separator_style = "round"
+    separator_style = "round",
   },
 
   hl_override = {
-  	Comment = { italic = true },
-  	["@comment"] = { italic = true },
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+    Normal = {
+      bg = "#1e1e2e",
+    },
   },
 }
 
